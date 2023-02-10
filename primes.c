e8b1ae11429a54ad8d0ab6c9214da53ad6900337
Felox <contact@felixlop.com>
2023-02-10 22:52:56 +0100

    different list struct, x7 faster

--- primes.c
+++ primes.c
@@ -1,6 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+unsigned sqrt(unsigned val) {
+    unsigned a = val, valDigits = 0;
+    do{
+        valDigits++;
+        a /= 10;
+    }
+    while(a != 0);
+    a = pow(10, valDigits/2);
+    for (int i=0; i<5; i++)
+    {
+        a = (a+val/a) /2;
+    }
+    return a;
+}
+
 typedef struct Element Element;
 struct Element
 {
@@ -13,50 +28,50 @@ struct Tab
     Element *first;
 };
 
-Tab *init(int firstElementValue)
+Tab *init(long nb)
 {
     Tab *tab = malloc(sizeof(Tab));
     Element *element = malloc(sizeof(Element));
     if (tab == NULL || element == NULL)
         exit(EXIT_FAILURE);
-    element->number = firstElementValue;
+    element->number = nb;
     element->next = NULL;
     tab->first = element;
     return tab;
 }
 
-void insertTab(Tab* tab, long nb)
+void insertTab(Tab *tab, long nb)
 {
+    Element *currentAddress = tab->first;
+    while (currentAddress->next != NULL) {
+        currentAddress = currentAddress->next;
+    }
     Element *newElement = malloc(sizeof(Element));
-    if (newElement == NULL || tab == NULL)
+    if (newElement == NULL)
         exit(EXIT_FAILURE);
     newElement->number = nb;
-    newElement->next = tab->first;
-    tab->first = newElement;
+    newElement->next = NULL;
+    currentAddress->next = newElement;
 }
 
 int scanTab(Tab *tab, long nb)
 {
     Element *currentElement = tab->first;
-    while (currentElement != NULL)
+    long squareRoot = sqrt(nb);
+    for (long i=1; i<squareRoot; i++)
     {
-        long rem = nb%(currentElement->number);
-        if (rem == 0)
-        {
-            currentElement = NULL;
+        if (nb%currentElement->number == 0) {
             return 0;
         }
         currentElement = currentElement->next;
     }
-    currentElement = NULL;
     return 1;
 }
 
 int main(int argc, char *argv[])
 {
     Tab *primes = init(2);
-
-    for (long i=3; i<500000; i+=2)
+    for (long i=3; i<500000; i++)
     {
         if (scanTab(primes, i) == 1)
         {
